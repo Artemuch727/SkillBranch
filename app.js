5974b7e41a582931c422f20350a349d8bbed6aa8
Artemuch727 <artem_270787@rambler.ru>
2016-11-08 20:13:55 +0300

    add new Task result (for SkillBranch Intensive) - TaskA - TaskB

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ var routes = require('./routes/index');
 var users = require('./routes/users');
 var task = require('./routes/task');
 
+
 var app = express();
 
 // view engine setup
--- routes/task.js
+++ routes/task.js
@@ -5,29 +5,67 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
+//
+// router.get('/', function(req, res, next) {
+//     var a = req.query.a;
+//     var b = req.query.b;
+//     var result = '';
+//
+//     if (isNaN(a) && isNaN(b)){
+//         result = 0;
+//     } else
+//     if (!isNaN(a) || !isNaN(b)) {
+//         console.log('1');
+//         if (!isNaN(a) && !isNaN(b)) {
+//             result = Number(a) + Number(b);
+//         } else {
+//             if (!isNaN(a)){
+//                 result = Number(a);
+//             } else if (!isNaN(b)){
+//                 result = Number(b);
+//             }
+//         }
+//     }
+//
+//     res.send(result.toString());
+// });
 
 router.get('/', function(req, res, next) {
-    var a = req.query.a;
-    var b = req.query.b;
-    var result = '';
+    var lastName, firstName, patroName, result;
+    var fullname = req.query.fullname;
+    var reg = /\d/;
 
-    if (isNaN(a) && isNaN(b)){
-        result = 0;
-    } else
-    if (!isNaN(a) || !isNaN(b)) {
-        console.log('1');
-        if (!isNaN(a) && !isNaN(b)) {
-            result = Number(a) + Number(b);
-        } else {
-            if (!isNaN(a)){
-                result = Number(a);
-            } else if (!isNaN(b)){
-                result = Number(b);
-            }
+    if (reg.test(fullname)){
+        res.send('Invalid fullname');
+    } else {
+
+        var resultArr = fullname.split(' ');
+
+        if (resultArr.length == 1) {
+            console.log(resultArr[0]);
+            lastName = resultArr[0];
+        } else if (resultArr.length == 2) {
+            lastName = resultArr[1];
+            firstName = resultArr[0];
+        } else if (resultArr.length == 3) {
+            lastName = resultArr[2];
+            firstName = resultArr[0];
+            patroName = resultArr[1];
         }
+
+        if (lastName && firstName && patroName) {
+            result = lastName + ' ' + firstName[0] + '. ' + patroName[0] + '.';
+        } else if (lastName && firstName) {
+            result = lastName + ' ' + firstName[0] + '.';
+        } else if (lastName) {
+            result = lastName;
+        } else {
+            result = 'Invalid fullname';
+        }
+        res.send(result);
     }
 
-    res.send(result.toString());
+
 });
 
 module.exports = router;
